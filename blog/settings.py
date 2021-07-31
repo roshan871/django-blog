d568d2b79cdad4630a443a3e364ab2086cb04ae5
roshan Dahal <roshandahal747@gmail.com>
2021-07-31 10:34:52 -0600

    django-heroku

--- blog/settings.py
+++ blog/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+) 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
